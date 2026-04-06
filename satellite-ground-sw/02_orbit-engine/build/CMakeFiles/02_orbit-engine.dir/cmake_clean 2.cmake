cb93caf1918f6ef34e6f5b263d38debb575c6361
KimDaJung <dajung@KimDaJungui-MacBookAir.local>
2026-04-06 20:15:29 +0900

    chore: 이미지 처리용 디렉터리 생성

---
+++ satellite-ground-sw/02_orbit-engine/build/CMakeFiles/02_orbit-engine.dir/cmake_clean 2.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "02_orbit-engine"
+  "02_orbit-engine.pdb"
+  "CMakeFiles/02_orbit-engine.dir/common/ReportWriter.cpp.o"
+  "CMakeFiles/02_orbit-engine.dir/common/ReportWriter.cpp.o.d"
+  "CMakeFiles/02_orbit-engine.dir/core/CoordinateConverter.cpp.o"
+  "CMakeFiles/02_orbit-engine.dir/core/CoordinateConverter.cpp.o.d"
+  "CMakeFiles/02_orbit-engine.dir/core/Vector3.cpp.o"
+  "CMakeFiles/02_orbit-engine.dir/core/Vector3.cpp.o.d"
+  "CMakeFiles/02_orbit-engine.dir/src/OrbitPropagator.cpp.o"
+  "CMakeFiles/02_orbit-engine.dir/src/OrbitPropagator.cpp.o.d"
+  "CMakeFiles/02_orbit-engine.dir/src/main.cpp.o"
+  "CMakeFiles/02_orbit-engine.dir/src/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/02_orbit-engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
